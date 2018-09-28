721c4cf57e80c5164fe95d4cd15b458dc7a0170d
songhailong <997496837@qq.com>
2018-09-28 14:47:21 +0800

    初始化

---
+++ StickFigureMK/Pods/Target Support Files/Pods-StickFigureMK/Pods-StickFigureMK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_StickFigureMK : NSObject
+@end
+@implementation PodsDummy_Pods_StickFigureMK
+@end
---
+++ StickFigureMK/StickFigureMK/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StickFigureMK
+//
+//  Created by 靓萌服饰靓萌服饰 on 2018/9/28.
+//  Copyright © 2018年 靓萌服饰靓萌服饰. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
